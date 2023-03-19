5adf7e0ed1154cccb32b65b3e321b50280719bc0
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-19 15:32:25 +0800

    init

---
+++ bin/ridesafe_cloud_database_impl.dart
@@ -0,0 +1,5 @@
+import 'package:ridesafe_cloud_database_impl/ridesafe_cloud_database_impl.dart' as ridesafe_cloud_database_impl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ridesafe_cloud_database_impl.calculate()}!');
+}
