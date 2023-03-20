7ec7ab0e4c38fd7295a87e9850886a140e9632d1
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-20 19:16:43 +0800

    linked deps

--- bin/ridesafe_cloud_database_impl.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ridesafe_cloud_database_impl/ridesafe_cloud_database_impl.dart' as ridesafe_cloud_database_impl;
-
-void main(List<String> arguments) {
-  print('Hello world: ${ridesafe_cloud_database_impl.calculate()}!');
-}
--- test/ridesafe_cloud_database_impl_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ridesafe_cloud_database_impl/ridesafe_cloud_database_impl.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
